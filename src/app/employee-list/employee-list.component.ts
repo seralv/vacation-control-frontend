55eec203b295dd26d8a00c53f3d5f54db62f1269
seralv <sergioalvaro.14@gmail.com>
2023-08-17 20:57:23 -0400

    Patched search filter

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -35,17 +35,15 @@ export class EmployeeListComponent implements OnInit {
   fetchEmployees(): void {
     this.employeeService.getEmployees().subscribe(data => {
       this.employees = data;
-      console.log('employees-->', this.employees);
 
+      this.vacationService.getVacations().subscribe(vacations => {
+        this.employees.forEach(employee => {
+          const employeeVacations = vacations.filter(vacation => vacation.employee === employee.id);
+          employee.vacation = employeeVacations;
+        });
 
-      // this.vacationService.getVacations().subscribe(vacations => {
-      //   this.employees.forEach(employee => {
-      //     const employeeVacations = vacations.filter(vacation => vacation.employee === employee.id);
-      //     employee.vacation = employeeVacations;
-      //   });
-
-        // this.applyFilter();
-      // });
+        this.applyFilter();
+      });
     });
   }
 
